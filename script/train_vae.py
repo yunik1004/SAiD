6cc5091d4c7a95ad4ba70b7f56be7024fb79ff24
inkyupark <inkyupark@krafton.com>
2023-09-07 18:36:30 +0900

    Fix: Restore default std_path

--- script/train_vae.py
+++ script/train_vae.py
@@ -288,7 +288,7 @@ def main():
     parser.add_argument(
         "--coeffs_std_path",
         type=str,
-        default="",  # (default_data_dir / "coeffs_std.csv").resolve(),  # "",
+        default=(default_data_dir / "coeffs_std.csv").resolve(),  # "",
         help="Path of the coeffs std data",
     )
     parser.add_argument(
