18e5fe63be7bbef1474904c34ebfeb3a9db93c83
inkyupark <inkyupark@krafton.com>
2023-09-08 23:42:04 +0900

    Fix: Remove unnecessary file

--- said/model/__init__.py
+++ said/model/__init__.py
@@ -1,9 +1,8 @@
-from . import diffusion, ldm, transformer, unet_1d_condition, vae, wav2vec2
+from . import diffusion, ldm, unet_1d_condition, vae, wav2vec2
 
 __all__ = [
     "diffusion",
     "ldm",
-    "transformer",
     "unet_1d_condition",
     "vae",
     "wav2vec2",
